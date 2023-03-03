69b5a2d243cc6bc77e9f39c14c42c4399ba05d1b
skumarlko2023 <skumarlko2023@gmail.com>
2023-03-04 00:30:46 +0530

    same question add.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {loading && <Loader />}
+      {!loading && !isQuizStarted && !isQuizCompleted && (
+        <Main startQuiz={startQuiz} />
+      )}
+      {!loading && isQuizStarted && (
+        <Quiz data={data} countdownTime={countdownTime} endQuiz={endQuiz} />
+      )}
+      {!loading && isQuizCompleted && (
+        <Result {...resultData} replayQuiz={replayQuiz} resetQuiz={resetQuiz} />
+      )}
+      <h1>Hello</h1>
+    </Layout>
   );
 }
 
